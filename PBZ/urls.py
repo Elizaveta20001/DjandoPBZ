bdb7b7d89ed756a69bd111b52d39c40016d05f78
Elizaveta <kovel.liza@mail.ru>
2020-10-14 01:04:47 +0300

    Added function for create product

--- PBZ/urls.py
+++ PBZ/urls.py
@@ -4,6 +4,7 @@ from PBZ import views
 urlpatterns = [
     path('',views.home_page),
     path('product_page',views.page_product,name='product_page'),
-    path('form_product',views.form_product,name='form_product')
+    path('form_product',views.form_product,name='form_product'),
+    path('create_product',views.create_product,name='create_product')
 
 ]
--- PBZ/views.py
+++ PBZ/views.py
@@ -11,3 +11,15 @@ def page_product(request):
 
 def form_product(request):
     return render(request,'form_product.html',{})
+
+def create_product(request):
+    if request.POST['name_product'] == '' or request.POST['category'] == '' or request.POST['company'] == '':
+        context = {}
+        context['error'] = True
+        return render(request,'form_product.html',context)
+    else:
+        name = request.POST['name_product']
+        category = request.POST['category']
+        company = request.POST['company']
+        Product(name=name,category=category,company=company).save()
+        return HttpResponseRedirect('/product_page')
