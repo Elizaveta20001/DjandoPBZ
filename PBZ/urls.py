36b2db19e2f7498bc82e09eab1d7a02b83ceca29
Elizaveta <kovel.liza@mail.ru>
2020-10-14 00:38:28 +0300

    Added page about product

--- PBZ/urls.py
+++ PBZ/urls.py
@@ -3,5 +3,6 @@ from PBZ import views
 
 urlpatterns = [
     path('',views.home_page),
+    path('product_page',views.page_product,name='product_page')
 
 ]
--- PBZ/views.py
+++ PBZ/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import HttpResponseRedirect
 
 def home_page(request):
     return render(request, 'home_page.html', {})
+
+def page_product(request):
+    return render(request,'page_product.html',{})
